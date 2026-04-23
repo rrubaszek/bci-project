38c60254b57d2429094562a21bdd1672cfc76ada
rrubaszek <robert.rubaszek@gmail.com>
2026-04-23 18:12:45 +0200

    added helper comment

--- src/paths.py
+++ src/paths.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
+"""
+Use this helper script to correctly place all models, plots etc.
+Keep it clean.
+"""
 SRC_DIR = Path(__file__).resolve().parent
 PROJECT_ROOT = SRC_DIR.parent
 
